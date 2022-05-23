c7d30b1f658d6aaa6f1406fc68cb4c1dbeede171
kmi <sageglorieux@hotmail.com>
2022-05-23 16:41:47 -0700

    refactor: useRef instead of useState for new QueryClient  On branch feature-01_context  Changes to be committed: 	modified:   src/pages/_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,6 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { Hydrate } from 'react-query/hydration';
-
-// import { ReactQueryDevtools } from 'react-query/devtools';
 
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -15,8 +12,7 @@ import { SEO } from '../components/SEO';
 import '../styles/globals.css';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  // const queryClient = React.useState(() => new QueryClient());
-  const queryClient = new QueryClient();
+  const queryClient = React.useRef(new QueryClient());
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
@@ -28,22 +24,20 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
 
   return (
     <ThemeProvider theme={theme}>
-      <QueryClientProvider client={queryClient}>
-        <Hydrate state={pageProps.dehydratedState}>
-          <Head>
-            <meta
-              name="viewport"
-              content="minimum-scale=1, initial-scale=1, width=device-width"
-            />
-            <SEO />
-          </Head>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
-          <Component {...pageProps} />
-          {/* 
+      <QueryClientProvider client={queryClient.current}>
+        <Head>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width"
+          />
+          <SEO />
+        </Head>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+        {/* 
           <ReactQueryDevtools />
           */}
-        </Hydrate>
       </QueryClientProvider>
     </ThemeProvider>
   );
