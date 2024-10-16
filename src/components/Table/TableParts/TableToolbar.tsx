616ba69f0cc871679b93b2e82265837e3df8a31a
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-10-16 16:18:50 -0700

    refactor

--- src/components/Table/TableParts/TableToolbar.tsx
+++ src/components/Table/TableParts/TableToolbar.tsx
@@ -1,30 +1,12 @@
-import { Grid, Toolbar, Typography } from '@mui/material';
-import makeStyles from '@mui/styles/makeStyles';
-
-const useToolbarStyles = makeStyles(() => ({
-  root: {},
-  title: {
-    flex: '1 1 100%',
-  },
-}));
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 const TableToolbar = ({ rowCount }: { rowCount: number }) => {
-  const classes = useToolbarStyles();
-
   return (
-    <Toolbar className={classes.root}>
-      <Grid container>
-        <Grid item xs={12}>
-          <Typography
-            className={classes.title}
-            variant="h6"
-            id="tableTitle"
-            component="div"
-          >
-            {rowCount} Earthquakes
-          </Typography>
-        </Grid>
-      </Grid>
+    <Toolbar>
+      <Typography variant="h6" id="tableTitle" component="div" align="left">
+        {rowCount} Earthquakes
+      </Typography>
     </Toolbar>
   );
 };
