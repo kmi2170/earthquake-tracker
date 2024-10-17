0b829ac07f0b965d6d719d7bf6e9de2a818196e8
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-10-17 16:27:40 -0700

    refactor

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,17 +1,13 @@
-import { memo } from 'react';
-
 import { Typography } from '@mui/material';
 
 const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
-    <footer>
-      <Typography variant="h6" component="footer">
-        &copy; {year} Kemmei H. | Earthquake date by USGC
-      </Typography>
-    </footer>
+    <Typography variant="h6" component="footer" sx={{ marginTop: '0.5rem' }}>
+      &copy; {year} Kemmei H. | Earthquake date by USGC
+    </Typography>
   );
 };
 
-export default memo(Footer);
+export default Footer;
